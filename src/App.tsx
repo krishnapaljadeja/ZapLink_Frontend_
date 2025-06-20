a58dad0d92859afa1fc500666a36f21758e72370
krishna <jadejakrishnapal04@gmail.com>
2025-06-21 01:00:00 +0530

    analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import Customize from "./components/Customize";
 import HowItWorks from "./components/HowItWorks";
 import AboutUs from "./components/AboutUs";
 import ViewZap from "./components/ViewZap";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/upload" element={<UploadPage />}></Route>
-      <Route path="/customize" element={<Customize />}></Route>
-      <Route path="/how-it-works" element={<HowItWorks />}></Route>
-      <Route path="/about" element={<AboutUs />}></Route>
-      <Route path="/zaps/:shortId" element={<ViewZap />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/upload" element={<UploadPage />}></Route>
+        <Route path="/customize" element={<Customize />}></Route>
+        <Route path="/how-it-works" element={<HowItWorks />}></Route>
+        <Route path="/about" element={<AboutUs />}></Route>
+        <Route path="/zaps/:shortId" element={<ViewZap />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
